dd38a174ef1e9d07fdc1032c16748f1dd373b6d2
Eric So <me@eric.so>
2025-06-06 19:31:44 -0700

    add linting to the backend CI workflow (#10)
    * add linting to the backend CI workflow; fix lint error
    
    * fix tests: path resolution in test setup.ts file

--- backend/src/test/setup.ts
+++ backend/src/test/setup.ts
@@ -1,9 +1,12 @@
 import { resolve } from 'path';
 import { register } from 'tsconfig-paths';
+import { readFileSync } from 'fs';
 
 // Load the tsconfig.json
 const baseUrl = resolve(__dirname, '..');
-const { paths } = require('../../tsconfig.json').compilerOptions;
+const tsConfigPath = resolve(__dirname, '../../tsconfig.json');
+const tsConfig = JSON.parse(readFileSync(tsConfigPath, 'utf-8'));
+const { paths } = tsConfig.compilerOptions;
 
 // Register the path aliases
 register({
